a88eea2b0ecf1399dc5dbbdfa8b63d268f38b8ca
Lloyd Arnold <lloarn@gmail.com>
2020-04-29 17:39:54 +0100

    Add turtle file

---
+++ Turtle.java
@@ -0,0 +1,26 @@
+package tk.lloydarnold;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Random;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Turtle {
+  int x;
+  int y;
+  // Graphics2D g2d;
+
+  public Turtle(){
+    x = 0;
+    y = 0;
+  //  g2d = (Graphics2D) g.create();
+  }
+}
