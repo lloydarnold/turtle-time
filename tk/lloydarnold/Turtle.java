bf7ee625c4f62e27da2c8fcab844d1350caf9531
Lloyd Arnold <lloarn@gmail.com>
2020-04-30 15:19:34 +0100

    Add move method & enum of commands

--- tk/lloydarnold/Turtle.java
+++ tk/lloydarnold/Turtle.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.Random;
+import java.util.Math;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -16,18 +17,34 @@ import javax.swing.Timer;
 public class Turtle {
   int x;
   int y;
+  boolean pen;
   int angle;  // Bearing - 0-359 inclusive
   // Graphics2D g2d;
+
+  enum commands{
+    LT,
+    RT,
+    FD,
+    BK,
+    PU,
+    PD
+  }
 
   public Turtle(){
     x = 0;
     y = 0;
-  //  g2d = (Graphics2D) g.create();
+    pen = true;
   }
 
   private turn(int turn_factor){
     angle += turn_factor;
     angle = angle % 360;
+  }
+
+  private logical_move(int magnitude) {
+    Double rad = Math.toRadians();
+    x += magnitude * Math.sin(rad);
+    y += magnitude * Math.cos(rad);
   }
 
 }
