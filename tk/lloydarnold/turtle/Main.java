1433e3e5b0e4d08615d496eabeecc1ac1c570812
Lloyd Arnold <lloarn@gmail.com>
2020-05-02 12:33:37 +0100

    Create turtle and pass this into graphics constructor

--- tk/lloydarnold/turtle/Main.java
+++ tk/lloydarnold/turtle/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
   public static void main(String[] args){
     System.out.println("Kick it! ");
-    TurtleGraphics g = new TurtleGraphics();
-    g.test();
+    TurtleLogic[] turt = {new TurtleLogic()};
+    TurtleGraphics g = new TurtleGraphics(turt);
+    g.test(turt);
   }
 }
