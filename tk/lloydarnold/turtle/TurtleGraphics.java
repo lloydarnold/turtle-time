f50419ba69f5e3fe4789f7b28f0283c2ea46a587
Lloyd Arnold <lloarn@gmail.com>
2020-05-02 12:33:10 +0100

    Extend graphics window declaration

--- tk/lloydarnold/turtle/TurtleGraphics.java
+++ tk/lloydarnold/turtle/TurtleGraphics.java
@@ -1,18 +1,25 @@
 package tk.lloydarnold.turtle;
 
-import java.awt.BasicStroke;
 import java.awt.EventQueue;
+import java.awt.BorderLayout;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 
-class Surface extends JPanel {
+
+class DrawSurface extends JPanel {
 
     private void doDrawing(Graphics g, int x1, int y1, int x2, int y2) {
 
         Graphics2D g2d = (Graphics2D) g;
         g2d.drawLine(x1, y1, x2, y2);
+
+    }
+
+    private void initSpace(TurtleLogic[] panelMyTurtles) {
+      setFocusable(true);
 
     }
 
@@ -26,35 +33,39 @@ class Surface extends JPanel {
 
 public class TurtleGraphics extends JFrame {
 
-    TurtleLogic[] myTurtles = null;
+    TurtleLogic[] frameMyTurtles = null;
+    private JLabel statusbar;
 
-    public TurtleGraphics(TurtleLogic[] myTurtles) {
+    public TurtleGraphics(TurtleLogic[] frameMyTurtles) {
 
-        this.myTurtles = myTurtles;
+        this.frameMyTurtles = frameMyTurtles;
         initUI();
     }
 
     private void initUI() {
 
-        add(new Surface());
+        statusbar = new JLabel(" Welcome to BTEC Logo :)");
+        add(statusbar, BorderLayout.SOUTH);
+        DrawSurface dSurf = new DrawSurface();
+        add(dSurf);
 
-        setTitle("Turtle Time");
-        setSize(500, 500);
+        setSize(600, 400);
+        setTitle("Logo Simulator");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    private void drawLine(int x1, int y1, int x2, int y2) {
+    /* private void drawLine(int x1, int y1, int x2, int y2) {
       // Surface.drawLine();
-    }
+    } */
 
-    public static void test() {
+    public static void test(TurtleLogic[] frameMyTurtles) {
 
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
 
-                TurtleGraphics ex = new TurtleGraphics();
+                TurtleGraphics ex = new TurtleGraphics(frameMyTurtles);
                 ex.setVisible(true);
             }
         });
