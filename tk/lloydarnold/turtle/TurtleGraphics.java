69092b19de983636da562a343a6c88e30aa323d5
Lloyd Arnold <lloarn@gmail.com>
2020-05-03 13:19:17 +0100

    Fix logic error by assiging myTurtles a value

--- tk/lloydarnold/turtle/TurtleGraphics.java
+++ tk/lloydarnold/turtle/TurtleGraphics.java
@@ -9,13 +9,13 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 
-
 class TurtleGraphics extends JPanel {
 
   TurtleLogic[] myTurtles = null;
 
     public TurtleGraphics(TurtleLogic[] myTurtles){
       setFocusable(true);
+      this.myTurtles = myTurtles;
     }
 
     private void doDrawing(Graphics g) {
@@ -23,13 +23,13 @@ class TurtleGraphics extends JPanel {
       Graphics2D g2d = (Graphics2D) g;
       g2d.setPaint(new Color(150, 150, 150));
 
-      /* for (int i = 0; i < myTurtles.length ; i++ ) {
-        // g2d.fillRect(myTurtles[i].x - 20, myTurtles[i].y - 20, 40, 40);
+      for (int i = 0; i < myTurtles.length ; i++ ) {
+        g2d.fillRect(myTurtles[i].x - 5, myTurtles[i].y - 5, 10, 10);
         for (int j = 0; j < myTurtles[i].lines.size() ; ) {
           tempLine = myTurtles[i].lines.get(j);
           g2d.drawLine(tempLine[0], tempLine[1], tempLine[2], tempLine[3]);
         }
-      } */
+      }
     }
 
     @Override
