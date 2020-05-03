16f703001ca410b2891a07bf95b8be592603a329
Lloyd Arnold <lloarn@gmail.com>
2020-05-03 13:08:36 +0100

    Change Main class to extend JFrame.
    This is the overarching container for the rest of the project

--- tk/lloydarnold/turtle/Main.java
+++ tk/lloydarnold/turtle/Main.java
@@ -1,11 +1,45 @@
 package tk.lloydarnold.turtle;
 
-public class Main {
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-  public static void main(String[] args){
-    System.out.println("Kick it! ");
-    TurtleLogic[] turt = {new TurtleLogic()};
-    TurtleGraphics g = new TurtleGraphics(turt);
-    g.test(turt);
-  }
-}
+
+public class Main extends JFrame {
+
+    private JLabel statusbar;
+
+    public Main() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        statusbar = new JLabel(" Welcome to BTEC Logo :)");
+        add(statusbar, BorderLayout.SOUTH);
+        TurtleLogic[] turt = {new TurtleLogic()};
+        TurtleGraphics tG = new TurtleGraphics(turt);
+        add(tG);
+
+        setSize(600, 400);
+        setTitle("Logo Simulator");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+      public static void main(String[] args){
+        System.out.println("Kick it! ");
+
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+                Main ex = new Main();
+                ex.setVisible(true);
+            }
+        });
+
+      }
+    }
