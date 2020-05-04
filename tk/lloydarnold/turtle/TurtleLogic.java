382d1e0d0f8b035d961133e1d01f4bf566b7a50a
Lloyd Arnold <lloarn@gmail.com>
2020-05-04 13:21:22 +0100

    Introduce getters & setters, make attributes private

--- tk/lloydarnold/turtle/TurtleLogic.java
+++ tk/lloydarnold/turtle/TurtleLogic.java
@@ -4,16 +4,17 @@ import java.lang.Math;
 import java.util.ArrayList;
 
 public class TurtleLogic {
-  int x;
-  int y;
+  private int x;
+  private int y;
   boolean pen;
   boolean isSelected;
-  int angle;  // Bearing - 0-359 inclusive
+  private int angle;  // Bearing - 0-359 inclusive
   ArrayList<int[]> lines = new ArrayList<int[]>();
 
   public TurtleLogic(){
     x = 100;
     y = 100;
+    angle = 0;
     pen = true;
     isSelected = true;
   }
@@ -21,6 +22,7 @@ public class TurtleLogic {
   public TurtleLogic(int x, int y){
     x = x;
     y = y;
+    angle = 0;
     pen = true;
     isSelected = true;
   }
@@ -28,16 +30,16 @@ public class TurtleLogic {
   private void turn(int turn_factor){
     angle += turn_factor;
     angle = angle % 360;
-    if (angle<360) angle += 360;
+    if (angle<0) angle += 360;
   }
 
-  private void logical_move(int magnitude) {
+  public void logical_move(int magnitude) {
     Double rad = Math.toRadians(angle);
-    x += magnitude * (int)Math.sin(rad);
-    y += magnitude * (int)Math.cos(rad);
+    this.x = x + (int)(magnitude * Math.sin(rad));
+    this.y = y + (int)(magnitude * Math.cos(rad));
   }
 
-  private void forward(int magnitude) {
+  public void forward(int magnitude) {
     int[] fNewLine;
     fNewLine = new int[4];
     fNewLine[0] = this.x;
@@ -71,17 +73,30 @@ public class TurtleLogic {
     switch (operator) {
       case "FD":
         forward(operand);
+        break;
       case "BK":
         backward(operand);
+        break;
       case "LT":
         left(operand);
+        break;
       case "RT":
         right(operand);
+        break;
       case "PU":
         this.pen = false;
+        break;
       case "PD":
         this.pen = true;
+        break;
 
     }
   }
+
+  public void setX(int newX) {this.x = newX; }
+  public void setY(int newY) {this.y = newY; }
+  public int getX() { return this.x; }
+  public int getY() { return this.y; }
+  public int getAng() { return this.angle; }
+
 }
