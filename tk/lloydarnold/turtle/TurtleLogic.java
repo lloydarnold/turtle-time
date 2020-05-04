490051ef4ac93d73cb353474c52be4b7e79ee6c4
Lloyd Arnold <lloarn@gmail.com>
2020-05-04 15:13:35 +0100

    Remove co-ordinate setters -- property should be read only

--- tk/lloydarnold/turtle/TurtleLogic.java
+++ tk/lloydarnold/turtle/TurtleLogic.java
@@ -93,8 +93,6 @@ public class TurtleLogic {
     }
   }
 
-  public void setX(int newX) {this.x = newX; }
-  public void setY(int newY) {this.y = newY; }
   public int getX() { return this.x; }
   public int getY() { return this.y; }
   public int getAng() { return this.angle; }
