c84b0d88a3d183eea2207a5062c8dc9226b1b4e2
Lloyd Arnold <lloarn@gmail.com>
2020-05-04 14:10:33 +0100

    Add textArea component and change position of button component
    Components draw correctly on first time, but when button is clicked go 
    AWOL. Next step is to fix this lmao

--- tk/lloydarnold/turtle/Main.java
+++ tk/lloydarnold/turtle/Main.java
@@ -5,12 +5,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.JTextArea;
 
 
 public class Main extends JFrame {
 
     private JLabel statusbar;
     private JButton runCode;
+    private JTextArea commandBox;
 
     public Main() {
 
@@ -21,13 +23,18 @@ public class Main extends JFrame {
 
         statusbar = new JLabel(" Welcome to BTEC Logo :)");
         runCode = new JButton("Run Script");
+        commandBox = new JTextArea(10, 10);
+        commandBox.setBounds(10, 400, 600, 130);
+        runCode.setBounds(700, 420, 100, 100);
         add(statusbar, BorderLayout.NORTH);
-        add(runCode, BorderLayout.SOUTH);
+        add(runCode);
+        add(commandBox);
         TurtleLogic[] turt = {new TurtleLogic()};
         TurtleGraphics tG = new TurtleGraphics(turt, runCode);
+        tG.setBounds(10, 10, 580, 380);
         add(tG);
 
-        setSize(600, 400);
+        setSize(850, 600);
         setTitle("Logo Simulator");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
