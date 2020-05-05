dd421d5a2d606b6c2ebc4530f68a3a311d016d18
Lloyd Arnold <lloarn@gmail.com>
2020-05-05 14:42:47 +0100

    Command parser now fully functional
    Command parser reads each line of input, tokenises it and turns it into 
    form that TurtleLogic will accept. As of yet, no support for loops. This 
    will come.

--- tk/lloydarnold/turtle/CommandParser.java
+++ tk/lloydarnold/turtle/CommandParser.java
@@ -1,6 +1,7 @@
 package tk.lloydarnold.turtle;
 
 import javax.swing.JTextArea;
+import java.util.ArrayList;
 
 public class CommandParser {
 
@@ -11,31 +12,50 @@ public class CommandParser {
     System.out.println(input);
   }
 
-  public void processCommands(JTextArea textBox) {
+  public ArrayList<String[]> processCommands(JTextArea textBox) {
     String rawInput = textBox.getText();
     String[] splitInput = rawInput.split("\n");
     finalCommands.clear();
 
     for (int i = 0; i < splitInput.length ; i++) {
       finalCommands.add(cleanCommand(splitInput[i]) );
+      print(finalCommands.get(i)[0]);
     }
+    
+    return finalCommands;
   }
 
   private String[] cleanCommand(String rawLine) {
-    String[] tokens = line.split(" ");
+    String[] tokens = rawLine.split(" ");
     if (tokens.length > 2) {
       print("Uh oh bruddah bro -- you put too much on one line");
       return null;
     }
-    tokens = interpret(tokens)
+    tokens = interpret(tokens);
+    return tokens;
   }
 
   private String[] interpret(String[] comLine) {
     String operator = processOperator(comLine[0].toUpperCase());
+    String[] returnable;
+
+    if (operator == null) {
+      print("Sorry, I don't understand that instruction");
+      return null;
+
+    } else if (! (operator.equals("PU") || operator.equals("PD"))) {
+      String operand = processOperand(comLine[1]);
+      returnable = new String[] { operator, operand };
+
+    } else {
+      returnable = new String[] { operator };
+    }
+
+    return returnable;
   }
 
   private String processOperator(String operator){
-    switch (operator) {
+    switch (operator.strip()) {
       case "FD":
       case "FORWARD":
         return "FD";
@@ -56,8 +76,21 @@ public class CommandParser {
         return "PD";
       default:
         return null;
-
     }
+  }
+
+  private String processOperand(String operand) {
+      operand = operand.strip();
+      if (!isNumeric(operand)) {
+        print("Operands must be numeric");
+        return null;
+      } else {
+        return operand;
+      }
+  }
+
+  private static boolean isNumeric(String str) {
+    return str.matches("-?\\d+(\\.\\d+)?");
   }
 
 }
